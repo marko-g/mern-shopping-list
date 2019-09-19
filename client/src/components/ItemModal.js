ed75406f18687e71368cfe171770647186bc0104
Your Name <you@example.com>
2019-09-20 01:13:35 +0200

    redux

---
+++ client/src/components/ItemModal.js
@@ -0,0 +1,89 @@
+import React, { Component } from "react";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Form,
+  FormGroup,
+  Label,
+  Input
+} from "reactstrap";
+import { connect } from "react-redux";
+import { addItem } from "../actions/itemActions";
+import uuid from "uuid";
+
+class ItemModal extends Component {
+  state = {
+    modal: false,
+    name: ""
+  };
+
+  toggle = () => {
+    this.setState({
+      modal: !this.state.modal
+    });
+  };
+
+  onChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+
+    const newItem = {
+      id: uuid(),
+      name: this.state.name
+    };
+
+    this.props.addItem(newItem);
+
+    this.toggle();
+  };
+
+  render() {
+    return (
+      <div>
+        <Button
+          color="dark"
+          style={{ marginBottom: "2rem" }}
+          onClick={this.toggle}
+        >
+          Add Item
+        </Button>
+        <Modal isOpen={this.state.modal} toggle={this.toggle}>
+          <ModalHeader toggle={this.toggle}>Add To Shopping List</ModalHeader>
+          <ModalBody>
+            <Form onSubmit={this.onSubmit}>
+              <FormGroup>
+                <Label form="item">Item</Label>
+                <Input
+                  type="type"
+                  name="name"
+                  id="item"
+                  placeholder="Add shopping item"
+                  onChange={this.onChange}
+                />
+                <Button color="dark" style={{ marginTop: "2rem" }} block>
+                  Add Item
+                </Button>
+              </FormGroup>
+            </Form>
+          </ModalBody>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  item: state.item
+});
+
+export default connect(
+  mapStateToProps,
+  { addItem }
+)(ItemModal);
